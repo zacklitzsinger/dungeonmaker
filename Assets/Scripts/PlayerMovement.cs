e61035be368376b26ac22964a69ab8db1a0c58c1
Zack Litzsinger <zelitzsi@ncsu.edu>
2017-02-04 23:00:33 -0800

    Added basic roll
    Player can right click to roll. Rolling is a tapering force applied in
    the direction of movement at the time of the start of the roll and
    prevents further movement untl after it is done. Rolling takes a certain
    number of frames to complete.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
     Rigidbody2D rb2d;
+    public LevelEditor editor;
     public float acceleration;
+    public float rollFrames; // Number of frames it takes to roll
+    public float rollForce; // Force with which to roll
+
+    public float remRollFrames; // Remaining frames to continue rolling; 0 unless rolling
 
     public int keys = 0;
 
@@ -18,7 +23,26 @@ public class PlayerMovement : MonoBehaviour {
     {
         float xMotion = Input.GetAxis("Horizontal");
         float yMotion = Input.GetAxis("Vertical");
-        Vector2 targetForce = Vector2.right * xMotion * acceleration + Vector2.up * yMotion * acceleration;
-        rb2d.AddForce(targetForce);
+        Vector2 targetMotion = Vector2.right * xMotion + Vector2.up * yMotion;
+
+        if (remRollFrames > 0)
+        {
+            rb2d.AddForce(targetMotion.normalized * rollForce / (rollFrames - remRollFrames + 1));
+            remRollFrames--;
+            return;
+        }
+        else
+        {
+            remRollFrames = 0;
+        }
+
+        if (Input.GetButtonDown("Roll"))
+        {
+            remRollFrames = rollFrames;
+        }
+        else
+        {
+            rb2d.AddForce(targetMotion * acceleration);
+        }
 	}
 }

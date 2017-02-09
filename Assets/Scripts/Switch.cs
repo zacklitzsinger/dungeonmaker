8d631a799c4659a67f8e08585d93c700f047215c
Zack Litzsinger <zelitzsi@ncsu.edu>
2017-02-08 20:32:59 -0800

    Added a switch object
    Switch object looks pressed when you walk over it. Currently doesn't do
    anything.

---
+++ Assets/Scripts/Switch.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour {
+
+    public bool active = false;
+
+    Animator animator;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        active = true;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        active = false;
+    }
+
+    void FixedUpdate()
+    {
+        animator.SetBool("active", active);
+
+    }
+}

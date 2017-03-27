f34ab3eadff0fe100be15a0351352ea4fab13b36
Zack Litzsinger <zelitzsi@ncsu.edu>
2017-03-27 14:28:33 -0700

    Added experimental skid effect, not used anywhere yet

---
+++ Assets/Scripts/SkidEffect.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class SkidEffect : MonoBehaviour {
+
+    public float threshold = 1;
+    public float factor = 1;
+    Vector2 lastVelocity;
+
+    public ParticleSystem effectParticleSystem;
+    Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponentInParent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 currentVelocity = rb2d.velocity;
+        float dot = Vector2.Dot(lastVelocity.normalized, currentVelocity.normalized);
+        if (threshold < Mathf.Abs(lastVelocity.magnitude - currentVelocity.magnitude * dot))
+        {
+            float amount = Mathf.Abs(lastVelocity.magnitude - currentVelocity.magnitude * dot);
+            amount *= factor;
+            effectParticleSystem.Emit(Mathf.FloorToInt(amount));
+        }
+        lastVelocity = currentVelocity;
+    }
+}

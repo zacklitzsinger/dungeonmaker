91a18c395f914a8ccfc08585558a1476c96ee3a9
Zack Litzsinger <zelitzsi@ncsu.edu>
2017-08-03 14:47:40 -0400

    Publish to github

---
+++ Assets/Scripts/WebServer.cs
@@ -0,0 +1,15 @@
+﻿/// <summary>
+/// Static collection of constants for communicating with the web server.
+/// </summary>
+public static class WebServer {
+
+    // WARNING: Remote server IP has been left out intentionally
+    public static string REMOTE_SERVER = "http://localhost:3000";
+    public static string LOCALHOST = "http://localhost:3000";
+#if UNITY_EDITOR
+    public static string SERVER = REMOTE_SERVER;
+#else
+    public static string SERVER = REMOTE_SERVER;
+#endif
+    public static string COOKIE = "";
+}
